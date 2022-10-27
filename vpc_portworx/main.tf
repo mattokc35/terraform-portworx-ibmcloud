8294b2f0c679deb69f5c11f4cbd27e5d23553294
Matthew Chen <mattokc35@ibm.com>
2022-10-27 11:21:53 -0500

    change source

--- vpc_portworx/main.tf
+++ vpc_portworx/main.tf
@@ -1,5 +1,5 @@
 module "portworx-enterprise" {
-  source  = "../"
+  source  = "https://github.com/mattokc35/terraform-portworx-ibmcloud.git"
   # insert the 3 required variables here
   region                    = "${var.region}"
   cluster_name              = "${var.cluster_name}"
